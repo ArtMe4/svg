930c0442d69c5839f363d35147fb73d61a8984b9
Artur <artur133715@gmail.com>
2019-11-09 12:02:12 +0600

    svg

---
+++ index.js
@@ -0,0 +1,19 @@
+function ring() {
+  let changeBG = document.querySelector("#changeR");
+  changeBG.style.fill = "#2d2dfd";
+}
+
+function ringR() {
+  let changeB = document.querySelector("#changeL");
+  changeB.style.fill = "#2d2dfd";
+}
+
+function right() {
+  let right = document.querySelector("#Layer_1");
+  right.style.animation = 'right 6s forwards';
+}
+
+function left() {
+  let left = document.querySelector("#Layer_1");
+  left.style.animation = 'left 6s forwards';
+}
